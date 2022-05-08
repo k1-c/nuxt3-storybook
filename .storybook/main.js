b6f92180bb9108013f9d46a915567b5772ed6b49
shunk-py <shun.kimura@integritis.io>
2022-05-08 15:12:41 +0900

    add sass module and settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,21 @@
+const { mergeConfig } = require('vite')
+
 module.exports = {
   stories: ['../components/stories/**/*.stories.mdx', '../components/stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
   framework: '@storybook/vue3',
   core: {
     builder: 'storybook-builder-vite',
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@import "../assets/styles/global.scss";',
+          },
+        },
+      },
+    })
   },
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/base.scss";',
+        },
+      },
+    },
+  },
 })
